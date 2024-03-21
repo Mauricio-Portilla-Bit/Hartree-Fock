f897bbfacff1d348022cc08f0d06e3a8a80eec23
marcelorg02 <minidomito@gmail.com>
2024-03-20 18:47:04 -0600

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 
-def Pl(n):      #Regresa los coeficietnes del enesimo polinomio de legendre
-    c = np.zeros([n+1,n+1])
+def Pl(n):      #Coeficients of a legendre polynomial
+    c = np.zeros([n+1,n+1])   
     c[0,0] = 1
     if n > 0:
         c[1,0] = 0
@@ -14,27 +14,33 @@ def Pl(n):      #Regresa los coeficietnes del enesimo polinomio de legendre
             cpre = np.delete(cpre,-1)
             c[i+1,:] =  ((2*i +1)*cpre-i*c[i-1][:])/(i+1)
     return c[n,:]
+#For every higher order polynomial lower order have to computed, carried out in this manner complexity bearly increases
+#cpre is the equivalent of multiplying Pn+1 by x, just carried out in coeficient notation
 
-def Polyval(c,x):    #Evalua los coeficietnes de un polinomio arbitrario en un vector 
+def Polyval(c,x): #Evaluates a "Polynomial vector" in a given domain
     v = np.zeros(len(x))
     for i in range(len(c)):
         print(c[i])
         v = v + c[i] * x**(i)
     return v
-def legendrep(n,x):
+#A little explanantion of what i mean with Polynomial vector (function) f(x) = 1+x+x^2 -----> (asociated vector) [1,1,1]
+#In this notation, derivatives, integrals,sums and multiplication by a a given vector are computationally friendly
+# I know, loops are not that optimal but to be fair, the value of c will rarely exceed 10
+ 
+def legendrep(n,x):   #ELegendre polynomials in a domain
     c = Pl(n) 
     y = Polyval(c, x)
     return y
+#Its just the evalutation using the other two functions
 
-
-def FAL(l,m,x): #Funcion que regresa las funciones asociadas de legendre
+def FAL(l,m,x): #It returns the associated legendre functions evaluated in the specified domain
  if m > l:
      print("error")
      return 1
  y = ((-1)**m)*((1-x**2)**(m/2))*Polyval(Polydiff(Pl(l),m),x)
  return y
 
-def Polydiff(c,m):   #Funion que calcula la derivada de un vector de coeficientes polinomial de orden mésimo
+def Polydiff(c,m):   #It calculates the mth order derivative of a given polynomial vector
     print(len(c))
     for i in range(m):
         c = np.delete(c,0)
@@ -42,6 +48,10 @@ def Polydiff(c,m):   #Funion que calcula la derivada de un vector de coeficiente
         t = len(c)
         c = c*(np.arange(t)+1)
     return c
+#Again, i know, loops, but seriously, when in the world will you take something higher than a 5th order 
+#dervative??
+
+
 
 x = np.linspace(-1,1,100)
 y = FAL(2,2,x)
