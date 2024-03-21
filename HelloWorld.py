6a2d846ce4a4812a23254d72f64027835f8c974c
marcelorg02 <minidomito@gmail.com>
2024-03-20 19:30:33 -0600

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
+from mpl_toolkits import mplot3d
 
 def Pl(n):      #Coeficients of a legendre polynomial
     c = np.zeros([n+1,n+1])   
@@ -23,11 +24,11 @@ def Polyval(c,x): #Evaluates a "Polynomial vector" in a given domain
         print(c[i])
         v = v + c[i] * x**(i)
     return v
-#A little explanantion of what i mean with Polynomial vector (function) f(x) = 1+x+x^2 -----> (asociated vector) [1,1,1]
-#In this notation, derivatives, integrals,sums and multiplication by a a given vector are computationally friendly
+#A little explanantion of what i mean with Polynomial vector : (function) f(x) = 1+x+x^2 -----> (asociated vector) [1,1,1]
+#In this notation, derivatives, integrals,sums and multiplication by a a given power of x are computationally friendly
 # I know, loops are not that optimal but to be fair, the value of c will rarely exceed 10
  
-def legendrep(n,x):   #ELegendre polynomials in a domain
+def legendrep(n,x):   #ELegendre polynomials in a domain c---> coefficient vector, x--->Domain
     c = Pl(n) 
     y = Polyval(c, x)
     return y
@@ -53,9 +54,37 @@ def Polydiff(c,m):   #It calculates the mth order derivative of a given polynomi
 
 
 
-x = np.linspace(-1,1,100)
-y = FAL(2,2,x)
-plt.plot(x,y)
+def SH(l,m,th,ph):  #It returns the spherical harmonics, evaluated in the corresponding domain
+    Nf = (((2*l+1)/(4*np.pi))*(np.math.factorial(l-np.abs(m))/np.math.factorial(l + np.abs(m))))**0.5 #Normalization constant 
+    Y = Nf*np.multiply(FAL(l,np.abs(m),np.cos(th)),np.exp(1j*m*ph)) #Actual calculation of the spherical harmonics
+    return Y
+#Important to note, theta is the angle with respect to the z axis ph is the azimuthal axis with respect to the x axis   
+
+
+th = np.linspace(0,np.pi,100)
+ph = np.linspace(0,np.pi*2,100)
+[TH,PH] = np.meshgrid(th,ph)
+X = np.multiply(np.cos(PH),np.sin(TH))
+Y = np.multiply(np.sin(PH),np.sin(TH))
+Z = np.cos(TH)
+f = np.real(SH(2,1,TH,PH))
+Xf = np.multiply(X,f)
+Yf = np.multiply(Y,f)
+Zf = np.multiply(Z,f)
+
+
+#Create figure
+fig = plt.figure(figsize =(14, 9))
+ax = plt.axes(projection ='3d')
+ 
+# Creating plot
+ax.plot_surface(Xf, Yf, Zf)
+ 
+# show plot
 plt.show()
+
+
+#just a polar plot to check that everything is fine
+
 
 
