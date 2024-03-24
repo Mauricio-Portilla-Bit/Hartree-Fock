a655a19a184032ed8ec8d9c9631b63b5586677d2
Mauricio Portilla Windows <portillarmz@gmail.com>
2024-03-23 18:28:47 -0600

    [Add] main implementation of random variables for radial distributions

--- main.py
+++ main.py
@@ -2,5 +2,27 @@ import numpy as np
 import Polarplots as p
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
+from statistic.pdf import pdf_generation
+from polynomial.laguerre import *
 
-p.Polarplots(4,1)
+
+#p.Polarplots(4,1)
+
+
+n = 3
+l = 2
+r = np.linspace(0,10,200)
+psi = -1*Radial_Function(n,l,r)
+f = 100*psi**2
+plt.plot(r, f*10)
+#plt.plot(r/a,(a**(2.9))*psi**2)
+#plt.xlim([0,18])
+#plt.ylim([-0.3,0.8])
+#plt.show()
+
+
+
+Y = pdf_generation(r,f, 1, 10000)
+print(Y)
+plt.hist(Y)
+plt.show()
--- polynomial/laguerre.py
+++ polynomial/laguerre.py
@@ -32,11 +32,16 @@ def Radial_Function(n: int, l: int, r):
         return 0
     
     a = 0.529 * 10 ** (-10)
+    a = 1
     Z = 1
     c = - np.sqrt(( (2*Z)/(n*a))**3 * (math.factorial(n - l - 1)/(2*n*(math.factorial(n + l))**3)))
     rho = 2*Z*r/(n*a)
     
     return c * np.multiply(np.multiply(np.exp(-rho/2), rho**(l)), Laguerre_Generalized(n + l, 2*l + 1, rho))
+
+
+"""
+Example of Implementation 
 
 a =  0.529 * 10 ** (-10)
 n = 2
@@ -49,3 +54,4 @@ plt.ylim([-0.3,0.8])
 plt.show()
 
 
+"""
